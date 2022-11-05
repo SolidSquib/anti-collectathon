819a66def8cc31f72caf65d3c65eea121c7bf1c5
Holystride <josh.pawson@googlemail.com>
2022-11-05 14:05:27 +0000

    Update HostileBehaviour.cs

--- Assets/HostileBehaviour.cs
+++ Assets/HostileBehaviour.cs
@@ -8,7 +8,14 @@ public class HostileBehaviour : MonoBehaviour
 
     public GameObject _Player = null;
     public float speed = 1f;
-    public float maxSpeed = 1f;
+    public float maxSpeed = 1f; 
+
+    public EBulletType m_desiredBulletType;
+    public bool _DieOnBulletHit = false;
+    public int _MaxBulletHits = 1;
+    public bool _DieAfterBulletHitDelay = false;
+    public float _DieDelay = 1f;
+    private int _TotalBulletImpacts = 0;
 
     private void FixedUpdate()
     {
@@ -37,5 +44,37 @@ public class HostileBehaviour : MonoBehaviour
     private void ApplyDirectionalForce(Vector2 force)
     {
         rb2D.AddForce(force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision detected");
+        Bullet bullet = collision.collider.GetComponent<Bullet>();
+        if (bullet)
+        {
+            Debug.Log("bullet collision detected");
+            if (bullet.m_bulletType == m_desiredBulletType)
+            {
+                // more score
+
+                _TotalBulletImpacts++;
+
+                // Do we want to kill the hostile after x Bullet Hits?
+                if (_DieOnBulletHit && _TotalBulletImpacts >= _MaxBulletHits)
+                {
+                    Destroy(gameObject);
+                }
+                // Do we want to despawn the hostile after x duration from a Bullet hit instead?
+                else if (_DieAfterBulletHitDelay)
+                {
+                    Destroy(gameObject, _DieDelay);
+
+                }
+            }
+            else
+            {
+                // score
+            }
+        }
     }
 }
